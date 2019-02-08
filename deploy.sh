fc0ee0889403f112e1098674448b9396d9a84f25
Lars Kellogg-Stedman <lars@redhat.com>
2019-02-08 10:19:07 -0500

    deploy.sh cleanups
    - we can disable nova compute by adding a resource_registry section to
      an existing environment file
    - keep backup files in local directory

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,33 @@
-## Create backup of modified packaged files and copy new versions
-
-# disable NovaCompute
-sudo cp files/standalone-tripleo.yaml.ironic /usr/share/openstack-tripleo-heat-templates/environments/standalone/standalone-tripleo.yaml
-
-# disable ftype check
-sudo cp /usr/share/ansible/roles/container-registry/tasks/docker.yml /usr/share/ansible/roles/container-registry/tasks/docker.yml.orig
-sudo cp files/docker.yml.noftypecheck /usr/share/ansible/roles/container-registry/tasks/docker.yml
-
-## Deploy
+#!/bin/sh
 
 export NETWORK=192.168.1
 export IP=$NETWORK.2
 export NETMASK=24
 export INTERFACE=em2
 
+## Create backup of modified packaged files and copy new versions
+
+mkdir -p backup
+
+# disable ftype check
+
+if [ ! -f backup/container-registry_tasks_docker.yml ]; then
+	cp /usr/share/ansible/roles/container-registry/tasks/docker.yml \
+		backup/container-registry_tasks_docker.yml
+fi
+
+sudo cp files/docker.yml.noftypecheck \
+	/usr/share/ansible/roles/container-registry/tasks/docker.yml
+
+## Deploy
+
 openstack tripleo container image prepare default \
   --output-env-file ./containers-prepare-parameters.yaml
 
+mkdir -p deploy
+
 sudo openstack tripleo deploy \
-  --templates \
+  --templates $TEMPLATES \
   --local-ip=$IP/$NETMASK \
   -e /usr/share/openstack-tripleo-heat-templates/environments/standalone/standalone-tripleo.yaml \
   -r /usr/share/openstack-tripleo-heat-templates/roles/Standalone.yaml \
@@ -27,6 +36,6 @@ sudo openstack tripleo deploy \
   -e /usr/share/openstack-tripleo-heat-templates/environments/services/ironic-inspector.yaml \
   -r /usr/share/openstack-tripleo-heat-templates/roles/Standalone.yaml \
   -e ./standalone_parameters.yaml \
-  --output-dir . \
+  --output-dir deploy \
   --standalone \
   "$@"
