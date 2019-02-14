df8195aee04342cd61bfb588b79405dedb31d42b
Lars Kellogg-Stedman <lars@redhat.com>
2019-02-14 13:56:58 -0500

    not clear if ubuntu needs the same grub changes as centos or not

--- images/ubuntu-bionic-18.04/configure-image.sh
+++ images/ubuntu-bionic-18.04/configure-image.sh
@@ -7,3 +7,8 @@ apt-get clean
 
 echo "ISCSI_AUTO=true" > /etc/iscsi/iscsi.initramfs
 update-initramfs -u
+
+sed -i '/GRUB_CMDLINE_LINUX/ s/"$/ rd.iscsi.firmware=1"/' \
+	/etc/default/grub
+
+update-grub
