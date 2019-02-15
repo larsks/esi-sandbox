53a6ed8707d46d894c7eaffacfa615f0730813ed
Lars Kellogg-Stedman <lars@redhat.com>
2019-02-14 20:45:26 -0500

    cloud-init isn't half as clever as it thinks
    cloud-init refuses to utilize the openstack data source on a bare
    metal server.  See https://bugs.launchpad.net/cloud-init/+bug/1815990
    for details.

--- images/ubuntu-bionic-18.04/configure-image.sh
+++ images/ubuntu-bionic-18.04/configure-image.sh
@@ -12,3 +12,9 @@ sed -i '/GRUB_CMDLINE_LINUX/ s/"$/ rd.iscsi.firmware=1 ci.ds=OpenStack"/' \
 	/etc/default/grub
 
 update-grub
+
+# Fix a bug in cloud-init that prevents it from running on a baremetal server
+# https://bugs.launchpad.net/cloud-init/+bug/1815990
+sed -i '/^ *def detect_openstack/ a\
+    return True
+' /usr/lib/python3/dist-packages/cloudinit/sources/DataSourceOpenStack.py
